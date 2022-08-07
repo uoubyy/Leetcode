6fd14bf0b69d7e92e6d4fe21253bb07fa2085a0b
uoubyy <uoubyy@gmail.com>
2022-08-07 13:49:39 -0600

    update common algorithm

--- env.h
+++ env.h
@@ -1,6 +1,7 @@
 #ifndef _ENV_H
 #define _ENV_H
 
+#include <array>
 #include <vector>
 #include <set>
 #include <map>
@@ -61,5 +62,131 @@ struct TreeNode {
 	TreeNode(int x, TreeNode* left, TreeNode* right)
 		: val(x), left(left), right(right) {}
 };
+
+TreeNode* BuildBinarySearchTree(vector<int>& nums) {
+	int n = nums.size();
+	if (n <= 0)
+		return nullptr;
+
+	TreeNode* root = new TreeNode(nums[0]);
+	queue<TreeNode*> nodes;
+	nodes.push(root);
+
+	int i = 1;
+	while (!nodes.empty())
+	{
+		TreeNode* curr = nodes.front();
+		nodes.pop();
+
+		TreeNode* left = i < n ? new TreeNode(nums[i]) : nullptr;
+		TreeNode* right = i + 1 < n ? new TreeNode(nums[i + 1]) : nullptr;
+
+		i += 2;
+		curr->left = left;
+		curr->right = right;
+
+		if (left != nullptr)
+			nodes.push(left);
+		if (right != nullptr)
+			nodes.push(right);
+	}
+
+	return root;
+}
+
+vector<vector<int>> muliMatrix(const vector<vector<int>>& mat1,
+							   const vector<vector<int>>& mat2) {
+	int m1 = mat1.size(), n1 = mat1[0].size(), m2 = mat2.size(),
+		n2 = mat2[0].size();
+
+	assert(n1 == m2);
+
+	vector<vector<int>> res(m1, vector<int>(n2, 0));
+	for (int i = 0; i < m1; ++i)
+	{
+		for (int j = 0; j < n2; ++j)
+		{
+			for (int k = 0; k < m2; ++k)
+				res[i][j] += mat1[i][k] * mat2[k][j];
+		}
+	}
+
+	return res;
+}
+
+// 10^75 = 10^(64 + 8 + 2 + 1)
+// 75 = 1001011
+
+vector<vector<int>> matrixPower(const vector<vector<int>>& mat, int k) {
+	int m = mat.size(), n = mat[0].size();
+	assert(m == n);
+
+	vector<vector<int>> res(m, vector<int>(m, 0));
+	for (int i = 0; i < m; ++i)
+		res[i][i] = 1;
+
+	vector<vector<int>> tmp(mat);
+
+	for (; k != 0; k >>= 1)
+	{
+		if ((k & 1) != 0)
+			res = muliMatrix(res, tmp);
+		tmp = muliMatrix(tmp, tmp);
+	}
+
+	return res;
+}
+
+namespace KMP
+{
+vector<int> getNextArray(const string& str) {
+	int n = str.length();
+	if (n == 1)
+		return {-1};
+
+	vector<int> next(n, 0);
+	next[0] = -1;
+	next[1] = 0;
+
+	int pos = 2;
+	int cn = 0;
+	while (pos < n)
+	{
+		if (str[pos - 1] == str[cn])
+			next[pos++] = ++cn;
+		else if (cn > 0)
+			cn = next[cn];
+		else
+			next[pos++] = 0;
+	}
+	return next;
+}
+
+int getIndexOf(const string& str, const string& match) {
+	int m = str.length(), n = match.length();
+
+	if (m <= 0 || n <= 0 || m < n)
+		return -1;
+
+	vector<int> next = getNextArray(match);
+
+	int si = 0, mi = 0;
+	while (si < m && mi < n)
+	{
+		if (str[si] == match[mi])
+		{
+			si++;
+			mi++;
+		}
+		else if (next[mi] == -1)
+		{ si++; }
+		else
+		{ mi = next[mi]; }
+	}
+
+	return mi == n ? si - mi : -1;
+}
+
+}  // namespace KMP
 
 #endif
--- sort.h
+++ sort.h
@@ -2,43 +2,79 @@
 
 using namespace std;
 
-void quick_sort(vector<int>& nums, int left, int right)
-{
-    if (left + 1 >= right)
-        return;
+void quick_sort(vector<int>& nums, int left, int right) {
+	if (left + 1 >= right)
+		return;
 
-    int l = left, r = right - 1, key = nums[l];
+	int l = left, r = right - 1, key = nums[l];
 
-    while (l < r) {
-        while (l < r && nums[r] >= key)
-            r--;
-        nums[l] = nums[r];
-        while (l < r && nums[l] <= key)
-            ++l;
-        nums[r] = nums[l];
-    }
-    nums[l] = key;
-    quick_sort(nums, left, l);
-    quick_sort(nums, l + 1, right);
+	while (l < r)
+	{
+		while (l < r && nums[r] >= key)
+			r--;
+		nums[l] = nums[r];
+		while (l < r && nums[l] <= key)
+			++l;
+		nums[r] = nums[l];
+	}
+	nums[l] = key;
+	quick_sort(nums, left, l);
+	quick_sort(nums, l + 1, right);
 }
 
-void merge_sort(vector<int>& nums, int left, int right, vector<int>& tmp)
-{
-    if (left + 1 >= right)
-        return;
+void merge_sort(vector<int>& nums, int left, int right, vector<int>& tmp) {
+	if (left + 1 >= right)
+		return;
 
-    int mid = left + (right - left) / 2;
-    merge_sort(nums, left, mid, tmp);
-    merge_sort(nums, mid, right, tmp);
+	int mid = left + (right - left) / 2;
+	merge_sort(nums, left, mid, tmp);
+	merge_sort(nums, mid, right, tmp);
 
-    int i = left, j = mid, k = left;
-    while (i < mid || j < right) {
-        if (j >= right || (i < mid && nums[i] < nums[j]))
-            tmp[k++] = nums[i++];
-        else
-            tmp[k++] = nums[j++];
-    }
+	int i = left, j = mid, k = left;
+	while (i < mid || j < right)
+	{
+		if (j >= right || (i < mid && nums[i] < nums[j]))
+			tmp[k++] = nums[i++];
+		else
+			tmp[k++] = nums[j++];
+	}
 
-    for (int i = left; i < right; ++i)
-        nums[i] = tmp[i];
+	for (int i = left; i < right; ++i)
+		nums[i] = tmp[i];
+}
+
+void insert_sort(vector<int>& nums, int n) {
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = i; j > 0 && nums[j] > nums[j - 1]; --j)
+		{ swap(nums[j], nums[j - 1]); }
+	}
+}
+
+void shell_sort(vector<int>& nums, int n) {
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+		for (int i = gap; i < n; ++i)
+		{
+			int temp = nums[i];
+			int j;
+			for (j = i; j >= gap && nums[j - gap] > temp; j -= gap)
+				nums[j] = nums[j - gap];
+			nums[j] = temp;
+		}
+	}
+}
+
+void selection_sort(vector<int>& nums, int n) {
+	int mid;
+	for (int i = 0; i < n - 1; ++i)
+	{
+		mid = i;
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (nums[j] < nums[mid])
+				mid = j;
+		}
+		swap(nums[mid], nums[i]);
+	}
 }
